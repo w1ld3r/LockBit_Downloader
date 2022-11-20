8bb8a4be39b0e3a47e4dd4422d8bfafd2a2b0482
w1ld3r <alientgp@hotmail.fr>
2022-11-20 01:15:54 +0100

    file_tree as variable

--- downloader_v2.py
+++ downloader_v2.py
@@ -4,13 +4,14 @@ import time
 
 import requests
 
+file_tree = "files_tree_f6edcd38e9.txt"
 base_url = "http://lockbitapt2yfbt7lchxejug47kmqvqqxvvjpqkmevv4l3azl3gy6pyd.onion/ajax/listing-post?file-download=true&post=AZl2fuI5CRGPoyoC63223bd7b0dd3&path="
 
 session = requests.session()
 session.proxies = {}
 session.proxies['http'] = 'socks5h://localhost:9050'
 
-with open("files_tree_f6edcd38e9.txt", "r") as f:
+with open(file_tree, "r") as f:
     for line in f.readlines():
         path = line.split(" - ", 1)[0][1:]
         url = f"{base_url}{path}"
