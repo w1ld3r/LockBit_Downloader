8a5f94e675b3c1532b4b353e6b6a3b774b33fe84
w1ld3r <alientgp@hotmail.fr>
2022-11-21 18:21:40 +0100

    remove use of deprecated params

--- app/scripts/format-import.sh
+++ app/scripts/format-import.sh
@@ -2,5 +2,5 @@
 set -x
 
 # Sort imports one per line, so autoflake can remove unused imports
-isort --recursive  --force-single-line-imports --apply src
+isort --force-single-line-imports src
 sh ./scripts/format.sh
